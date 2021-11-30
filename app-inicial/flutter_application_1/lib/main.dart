02a73937d87c52c4e6bd71a4e1f6608a390d0344
gilouzi <giovannabellonia@gmail.com>
2021-11-30 14:31:07 -0300

    adicionando um item

--- app-inicial/flutter_application_1/lib/main.dart
+++ app-inicial/flutter_application_1/lib/main.dart
@@ -40,39 +40,57 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var newTaskController = TextEditingController();
+
+  void add() {
+    if (newTaskController.text.isEmpty) return;
+    setState(() {
+      widget.items.add(Item(
+        title: newTaskController.text,
+        done: false,
+      ));
+      newTaskController.clear();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: TextFormField(
-            controller: newTaskController,
-            keyboardType: TextInputType.text,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-            ),
-            decoration: InputDecoration(
-                labelText: "Nova tarefa",
-                labelStyle: TextStyle(
-                  color: Colors.white,
-                )),
+      appBar: AppBar(
+        title: TextFormField(
+          controller: newTaskController,
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
           ),
+          decoration: InputDecoration(
+              labelText: "Nova tarefa",
+              labelStyle: TextStyle(
+                color: Colors.white,
+              )),
         ),
-        body: ListView.builder(
-          itemCount: widget.items.length,
-          itemBuilder: (BuildContext ctxt, int index) {
-            final item = widget.items[index];
-            return CheckboxListTile(
-              title: Text(item.title),
-              key: Key(item.title),
-              value: item.done,
-              onChanged: (value) {
-                setState(() {
-                  item.done = value;
-                });
-              },
-            );
-          },
-        ));
+      ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext ctxt, int index) {
+          final item = widget.items[index];
+          return CheckboxListTile(
+            title: Text(item.title),
+            key: Key(item.title),
+            value: item.done,
+            onChanged: (value) {
+              setState(() {
+                item.done = value;
+              });
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: add,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.pink,
+      ),
+    );
   }
 }
