77ed4000121a85070eede7f85cc96b14de85f74d
gilouzi <giovannabellonia@gmail.com>
2021-11-30 12:02:48 -0300

    list view

--- app-inicial/flutter_application_1/lib/main.dart
+++ app-inicial/flutter_application_1/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
+import 'models/item.dart';
+
 void main() {
   runApp(App());
 }
@@ -22,20 +24,32 @@ class App extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   //const HomePage({ Key? key }) : super(key: key);
+  var items = [];
 
+  HomePage() {
+    items.add(Item(title: "Item 1", done: false));
+    items.add(Item(title: "Item 2", done: true));
+    items.add(Item(title: "Item 3", done: false));
+  }
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Todo List"),
-      ),
-      body: Container(
-        child: Center(
-          child: Text("Olá mundo"),
+        appBar: AppBar(
+          title: Text("Todo List"),
         ),
-      ),
-    );
+        body: ListView.builder(
+          itemCount: widget.items.length,
+          itemBuilder: (BuildContext ctxt, int index) {
+            return Text(widget.items[index].title);
+          },
+        ));
   }
 }
